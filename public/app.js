23b54479d7db63e456406722218a6e7955232619
Morgan Pitt <morgan.pitt@gmail.com>
2016-12-02 17:26:22 -0500

    added animations

--- public/app.js
+++ public/app.js
@@ -1,12 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import './animate.css';
 import './monsoon.css';
 
 import angular from 'angular';
 import uirouter from 'angular-ui-router';
+import ngAnimate from 'angular-animate'
 
 import routes from './app.routes';
 
 import home from './components/home/home.controller';
 
-angular.module('app', [uirouter])
+angular.module('app', [uirouter, ngAnimate])
   .config(routes);
--- public/components/app/app.controller.js
+++ public/components/app/app.controller.js
@@ -1,19 +1,22 @@
-appCtrl.$inject = ['$interval'];
-export default function appCtrl($interval) {
+appCtrl.$inject = ['$interval', '$timeout'];
+export default function appCtrl($interval, $timeout) {
     console.log('appCtrl');
     let vm = this;
-    vm.appBg = {
-        "background-image": ""
-    };
+    vm.bgImg = 'img/bg1.jpg';
 
     function changeBackground() {
         console.log('changing image');
+        vm.imgLoaded = false;
         let imageNum = Math.floor(Math.random() * 6) + 1;
-        vm.appBg['background-image'] = 'url("img/bg' + imageNum + '.jpg")'; 
+        vm.bgImg = 'img/bg' + imageNum + '.jpg'; 
+        $timeout(()=>{
+            vm.imgLoaded = true;
+        }, 50)
+
     }
     changeBackground();
     $interval(()=>{
         changeBackground()
-    }, 15000);
+    }, 10000);
 
 }
