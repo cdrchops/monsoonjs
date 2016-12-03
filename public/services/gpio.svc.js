fe59528132fe071f58352641d048fe7bdf94e299
Morgan Pitt <morgan.pitt@gmail.com>
2016-12-03 15:52:17 -0500

    fix typo on gpiosvc parameters

--- public/services/gpio.svc.js
+++ public/services/gpio.svc.js
@@ -3,7 +3,7 @@
 gpioSvc.$inject = ['$resource'];
 
 function gpioSvc($resource){
-    return $resource('/api/gpio/:pin/:acion', {pin:'@pin', action:'@action'}, {
+    return $resource('/api/gpio/:pin/:action', {pin:'@pin', action:'@action'}, {
         'set': {method:'POST'}
     })
 }
